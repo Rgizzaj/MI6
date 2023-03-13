03278dceb958a8e59cf9d4b083153314622cccd3
Kurt Rode <kurtrode@seznam.cz>
2023-03-13 13:24:38 +0100

    Added relationships to Alias model

--- app/Models/Alias.php
+++ app/Models/Alias.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Alias extends Model
 {
     use HasFactory;
+
+    public function people()
+    {
+        return $this->belongsTo((Person::class));
+    }
 }
